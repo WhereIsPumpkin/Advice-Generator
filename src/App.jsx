db4b497bd24f36e9019f59ce9bdbe56022b94427
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-03-28 00:58:55 +0400

    id function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import dividerDesktop from "./images/pattern-divider-desktop.svg";
 import diceIcon from "./images/icon-dice.svg";
 import styles from "./App.module.scss";
 
 function App() {
+  const [id,setId] = useState("")
+  const [advice,setAdivce] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        setId(data.slip.id)
+      })
+  }, []);
+
   return (
     <div className={styles.container}>
-      <span className={styles.adviceNumber}>ADVICE #</span>
-      <h1 className={styles.adviceText}>
-        
-      </h1>
+      <span className={styles.adviceNumber}>ADVICE #{id}</span>
+      <h1 className={styles.adviceText}>{advice}</h1>
       <img src={dividerDesktop} alt="dividerLine" />
       <div className={styles.randomizeButton}>
         <img src={diceIcon} alt="diceIcon" />
