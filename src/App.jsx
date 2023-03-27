1d5312bba742ad32c9ae580afc75b9ae4b87cc5a
WhereIsPumpkin <sgogr2021@agruni.edu.ge>
2023-03-28 01:16:22 +0400

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import dividerDesktop from "./images/pattern-divider-desktop.svg";
+import dividerMobile from "./images/pattern-divider-mobile.svg";
 import diceIcon from "./images/icon-dice.svg";
 import styles from "./App.module.scss";
 
@@ -24,7 +25,9 @@ function App() {
     <div className={styles.container}>
       <span className={styles.adviceNumber}>ADVICE #{id}</span>
       <h1 className={styles.adviceText}>{advice}</h1>
-      <img src={dividerDesktop} alt="dividerLine" />
+      <img src={dividerDesktop} alt="dividerLine" className={styles.desktopDiv}/>
+      <img src={dividerMobile} alt="dividerLine" className={styles.mobileDiv}/>
+
       <div
        className={styles.randomizeButton}
        onClick={fetchAdvice}
